bfdd1624c42281afd4dcc89dc2bd5cb814150226
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2026-09-13 21:12:58 +0300

    Implement servo control and sugar sensor logic
    Added servo control and sugar sensor reading functionality.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,42 @@
+#include <ESP32Servo.h>
 
-int main(){
-  return 0;
+const int servoPin = 18;
+
+Servo servo;
+
+void setup() {
+  Serial.begin(115200);
+  servo.attach(servoPin, 500, 2400);
 }
+
+int pos = 0;
+int insulinCC = 12;
+
+
+void loop() {
+  int sugerSens = map(analogRead(34),0,4095,0,500);
+
+  if(sugerSens >= 140 && sugerSens < 200){
+    Serial.print("Sug0: ");
+    Serial.println(sugerSens);
+  }
+
+  else if(sugerSens >= 200 && sugerSens < 300){
+    Serial.print("Sug1: ");
+    Serial.println(sugerSens);
+  }
+
+  else if(sugerSens >= 300 && sugerSens < 400){
+    Serial.print("Sug2: ");
+    Serial.println(sugerSens);
+  }
+
+  else if(sugerSens >= 400){
+    Serial.print("Sug3: ");
+    Serial.println(sugerSens);
+  }
+  
+
+
+}
+
