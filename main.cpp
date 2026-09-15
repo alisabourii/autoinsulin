c5c12d658dbbb466f2573bda97f40f049389f1e3
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2026-09-15 22:37:37 +0300

    Update servo control and simplify loop logic
    Refactored servo control logic and removed unused code.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,15 @@
 #include <ESP32Servo.h>
 
-const int servoPin = 18;
+Servo injector;
+const int servoPin = 18; // Sinyal kablosunun (turuncu/sarı) bağlı olduğu pin
+const int sugerInput = 34;
 
-Servo servo;
 
 void setup() {
-  Serial.begin(115200);
-  servo.attach(servoPin, 500, 2400);
+  injector.attach(servoPin); // Servoyu pine bağla
 }
-
-int pos = 0;
-int insulinCC = 12;
-
 
 void loop() {
-  int sugerSens = map(analogRead(34),0,4095,0,500);
-
-  if(sugerSens >= 140 && sugerSens < 200){
-    Serial.print("Sug0: ");
-    Serial.println(sugerSens);
-  }
-
-  else if(sugerSens >= 200 && sugerSens < 300){
-    Serial.print("Sug1: ");
-    Serial.println(sugerSens);
-  }
-
-  else if(sugerSens >= 300 && sugerSens < 400){
-    Serial.print("Sug2: ");
-    Serial.println(sugerSens);
-  }
-
-  else if(sugerSens >= 400){
-    Serial.print("Sug3: ");
-    Serial.println(sugerSens);
-  }
-  
-
-
+  int input = map(analogRead(sugerInput),0,4095,0,180);
+  injector.write(input);    // 0 dereceye git
 }
-
