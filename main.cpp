7c802b6b975935fea332ebed8309a64e2f168130
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2026-09-16 23:20:37 +0300

    Implement sugar sensor-based injector control
    Refactor loop to control injector based on sugar sensor readings.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,28 @@ Servo injector;
 const int servoPin = 18; // Sinyal kablosunun (turuncu/sarı) bağlı olduğu pin
 const int sugerInput = 34;
 
+int insulinMM = 10;
 
 void setup() {
   injector.attach(servoPin); // Servoyu pine bağla
 }
 
 void loop() {
-  int input = map(analogRead(sugerInput),0,4095,0,180);
-  injector.write(input);    // 0 dereceye git
+  int sugerSens = map(analogRead(sugerInput),0,4095,0,500);
+
+  if(sugerSens >= 140 && sugerSens < 200){
+    injector.write(45);
+  }
+
+  else if(sugerSens >= 200 && sugerSens < 300){
+    injector.write(90);
+  }
+
+  else if(sugerSens >= 300 && sugerSens < 400){
+    injector.write(135);
+  }
+
+  else if(sugerSens >= 400){
+    injector.write(180);
+  }
 }
